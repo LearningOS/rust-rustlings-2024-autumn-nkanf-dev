e46f73d95ecbaba171f02466b61e38e0a5ae9710
nkanf-dev <n.kan.f@outlook.com>
2024-10-18 09:39:59 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,25 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
+use std::mem;
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T: PartialOrd + Copy>(array: &mut [T]) {
+    let len = array.len();
+    for i in 0..len - 1 {
+        let mut min_index = i;
+        for j in i..=len - 1 {
+            if array[j] < array[min_index] {
+                min_index = j;
+            }
+        }
+        if min_index != i {
+            let temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +31,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
